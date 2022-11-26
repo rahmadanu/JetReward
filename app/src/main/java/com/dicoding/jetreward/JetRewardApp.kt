b7cb91348a826446098867289e72fc600c672683
rahmadanu <sadewarahmadanu@gmail.com>
2022-11-26 13:24:31 +0700

    Hide bottom nav in specific page

--- app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -32,9 +32,14 @@ fun JetRewardApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     Scaffold(
         bottomBar = {
-            BottomBar(navController)
+            if (currentRoute != Screen.DetailReward.route) {
+                BottomBar(navController)
+            }
         },
         modifier = modifier,
     ) { innerPadding ->
