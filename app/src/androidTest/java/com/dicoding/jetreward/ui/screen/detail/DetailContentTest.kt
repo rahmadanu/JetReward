963890c0d9ce1b92d82176b869cfa4077c4604b0
rahmadanu <sadewarahmadanu@gmail.com>
2022-11-27 16:03:16 +0700

    Add semantics to code and test some scenarios

--- app/src/androidTest/java/com/dicoding/jetreward/ui/screen/detail/DetailContentTest.kt
+++ app/src/androidTest/java/com/dicoding/jetreward/ui/screen/detail/DetailContentTest.kt
@@ -1,16 +1,17 @@
 package com.dicoding.jetreward.ui.screen.detail
 
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
 import com.dicoding.jetreward.R
 import com.dicoding.jetreward.model.OrderReward
 import com.dicoding.jetreward.model.Reward
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
+import onNodeWithStringId
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class DetailContentTest {
     @get:Rule
@@ -36,5 +37,24 @@ class DetailContentTest {
             }
         }
         composeTestRule.onRoot().printToLog("currentLabelExists")
+    }
+
+    @Test
+    fun detailContent_isDisplayed() {
+        composeTestRule.onNodeWithText(fakeOrderReward.reward.title).assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.required_point,
+                fakeOrderReward.reward.requiredPoint
+            )
+        ).assertIsDisplayed()
+    }
+
+    @Test
+    fun increaseProduct_buttonEnabled() {
+        composeTestRule.onNodeWithContentDescription("Order Button").assertIsNotEnabled()
+        composeTestRule.onNodeWithStringId(R.string.plus_symbol)
+            .performClick()
+        composeTestRule.onNodeWithContentDescription("Order Button").assertIsEnabled()
     }
 }
